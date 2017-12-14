01ab5ad029bdc1cd7616c0480524e042f34a76e0
Maxim <k.maxim107@gmail.com>
2017-12-14 20:23:21 +0200

    Added new result of tasks

--- js/main.js
+++ js/main.js
@@ -4,55 +4,77 @@ class LearningFrontend {
 
     static average(){
         let sum = 0;
-        const arr = prompt("Введите числа").split(",");
+        const arr = [2,4,5,12,23];
 
-        for(var i = 0; i < arr.length; i++){
-            sum += Number(arr[i]);
+        for(let i = 0; i < arr.length; i++){
+            sum += arr[i];
         }
 
-        sum = sum/arr.length;
-        console.info("Среднее арифметическое = "+ parseInt(sum));
+        sum = Number(sum/arr.length);
+
+        return sum;
     }
 
     static exchange(){
-        let axchange = Number(prompt("Введите значения в долларах"));
-        console.info("В "+ axchange +" долларах "+ axchange * 27 + " грн");
+        const axchange = 100;
+
+        return ("В "+ axchange +" долларах "+ axchange * 27 + " грн");
     }
 
     static workPermit(){
         let access = false;
-        const requirements = {};
-        requirements.firstName = String(prompt("Введите ваше имя"));
-        requirements.age = Number(prompt("Введите ваш возраст"));
-        requirements.doYouSmoke = Boolean(confirm("Курите ли вы?"));
-        requirements.expiarance = Number(prompt("Какой у вас опыт?"));
+        const employeInfo = {
+            firstName  : "Alex",
+            age        : 33,
+            doYouSmoke : false,
+            expiarance : 6
+        };
 
-        if( requirements.age >= 18 && !requirements.doYouSmoke && requirements.expiarance > 5){
+        if( employeInfo.age >= 18 && !employeInfo.doYouSmoke && employeInfo.expiarance > 5){
             access = true;
         }
 
         if(access){
-            console.info("У нас для вас "+ requirements.firstName +" хорошая новость.Вы подходите!");
-        }else{
-            console.info("Мы вам перезвоним :)");
+            return ("У нас для вас "+ employeInfo.firstName +" хорошая новость.Вы подходите!");
         }
 
+        return ("Мы вам перезвоним :)");
     }
 
     static isPalindrom(){
-        const str = String(prompt("Введите слово: "));
+        const str = "lol";
         const strReverse = str.split('').reverse().join('');
-        if(str == strReverse){
-            console.log("Это слово палиндром!");
-        }else{
-            console.log("Это обычное слово!")
-        }
+        let result = str === strReverse ? "Это слово палиндром!" : "Это обычное слово!";
+
+        return result;
     }
 
     static addOneIntoArray(){
-        let myarr = [55,234,22,123,654];
-        for (let i = 0; i < myarr.length; myarr[i] += 1, i++);
-        console.log(myarr);
+        let firstArr = [55,234,22,123,654];
+        let secondArr = firstArr.map(function(value){
+            return value+1;
+        });
+
+        return secondArr;
+    }
+
+    static returnIteration(num){
+        let i = num = num || 0;
+        let count = 0;
+        return {
+            iterateMethod : () => {
+                return i++, count++;
+            },
+            iterateCount : () => {
+                return "Счетчик начался с "+ num +", всего было произведенно итераций "+ count +" раз";
+            }
+        };
+    }
+
+    static transformToNumber(subj){
+        subj = isNaN(subj) ? true : subj;
+
+        return Number(subj);
     }
 }
 
